f9f922060a1564667a0f9558c8679e24730c5a0d
Richard Braun <rbraun@sceen.net>
2014-05-21 00:37:08 +0200

    test/test_llsync_defer: new test module

---
+++ test/test_llsync_defer.c
@@ -0,0 +1,200 @@
+/*
+ * Copyright (c) 2014 Richard Braun.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * This test module is a stress test, expected to never terminate, of the
+ * work deferring functionality of the llsync module. It creates three
+ * threads, a producer, a consumer, and a "peeker". The producer allocates
+ * a page and writes it. It then transfers the page to the consumer, using
+ * the llsync interface to update the global page pointer. Once at the
+ * consumer, the llsync interface is used to defer the release of the page.
+ * Concurrently, the peeker accesses the page and checks its content when
+ * available. These accesses are performed inside a read-side critical
+ * section and should therefore never fail.
+ *
+ * Each thread regularly prints a string to report that it's making progress.
+ */
+
+#include <kern/assert.h>
+#include <kern/condition.h>
+#include <kern/kmem.h>
+#include <kern/llsync.h>
+#include <kern/macros.h>
+#include <kern/mutex.h>
+#include <kern/panic.h>
+#include <kern/param.h>
+#include <kern/printk.h>
+#include <kern/stddef.h>
+#include <kern/string.h>
+#include <kern/thread.h>
+#include <kern/work.h>
+#include <test/test.h>
+#include <vm/vm_kmem.h>
+
+struct test_pdsc {
+    struct work work;
+    unsigned long va;
+};
+
+#define TEST_VALIDATION_BYTE 0xab
+
+static struct mutex test_lock;
+static struct condition test_condition;
+static struct test_pdsc *test_pdsc;
+
+static struct kmem_cache test_pdsc_cache;
+
+static void
+test_alloc(void *arg)
+{
+    struct test_pdsc *pdsc;
+    unsigned long i;
+
+    (void)arg;
+
+    i = 0;
+
+    mutex_lock(&test_lock);
+
+    for (;;) {
+        while (test_pdsc != NULL)
+            condition_wait(&test_condition, &test_lock);
+
+        pdsc = kmem_cache_alloc(&test_pdsc_cache);
+
+        if (pdsc != NULL) {
+            pdsc->va = vm_kmem_alloc(PAGE_SIZE);
+
+            if (pdsc->va != 0)
+                memset((void *)pdsc->va, TEST_VALIDATION_BYTE, PAGE_SIZE);
+        }
+
+        llsync_assign_ptr(test_pdsc, pdsc);
+        condition_signal(&test_condition);
+
+        if ((i % 100000) == 0)
+            printk("alloc ");
+
+        i++;
+    }
+}
+
+static void
+test_deferred_free(struct work *work)
+{
+    struct test_pdsc *pdsc;
+
+    pdsc = structof(work, struct test_pdsc, work);
+
+    if (pdsc->va != 0)
+        vm_kmem_free(pdsc->va, PAGE_SIZE);
+
+    kmem_cache_free(&test_pdsc_cache, pdsc);
+}
+
+static void
+test_free(void *arg)
+{
+    struct test_pdsc *pdsc;
+    unsigned long i;
+
+    (void)arg;
+
+    i = 0;
+
+    mutex_lock(&test_lock);
+
+    for (;;) {
+        while (test_pdsc == NULL)
+            condition_wait(&test_condition, &test_lock);
+
+        pdsc = test_pdsc;
+        llsync_assign_ptr(test_pdsc, NULL);
+
+        if (pdsc != NULL) {
+            work_init(&pdsc->work, test_deferred_free);
+            llsync_defer(&pdsc->work);
+        }
+
+        condition_signal(&test_condition);
+
+        if ((i % 100000) == 0)
+            printk("free ");
+
+        i++;
+    }
+}
+
+static void
+test_read(void *arg)
+{
+    const struct test_pdsc *pdsc;
+    const unsigned char *s;
+    unsigned long i, j;
+
+    (void)arg;
+
+    i = 0;
+
+    for (;;) {
+        llsync_read_enter();
+
+        pdsc = llsync_read_ptr(test_pdsc);
+
+        if (pdsc != NULL) {
+            s = (const unsigned char *)pdsc->va;
+
+            if (s != NULL) {
+                for (j = 0; j < PAGE_SIZE; j++)
+                    if (s[j] != TEST_VALIDATION_BYTE)
+                        panic("invalid content");
+
+                if ((i % 100000) == 0)
+                    printk("read ");
+
+                i++;
+            }
+        }
+
+        llsync_read_leave();
+    }
+}
+
+void
+test_setup(void)
+{
+    struct thread_attr attr;
+    struct thread *thread;
+    int error;
+
+    condition_init(&test_condition);
+    mutex_init(&test_lock);
+
+    kmem_cache_init(&test_pdsc_cache, "test_pdsc", sizeof(struct test_pdsc),
+                    0, NULL, NULL, NULL, 0);
+
+    thread_attr_init(&attr, "x15_test_alloc");
+    error = thread_create(&thread, &attr, test_alloc, NULL);
+    assert(!error);
+
+    thread_attr_init(&attr,"x15_test_free");
+    error = thread_create(&thread, &attr, test_free, NULL);
+    assert(!error);
+
+    thread_attr_init(&attr, "x15_test_read");
+    error = thread_create(&thread, &attr, test_read, NULL);
+    assert(!error);
+}
