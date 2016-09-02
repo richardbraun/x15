34f9c5d0836a870f902471e299b0adee114f08be
Richard Braun <rbraun@sceen.net>
2016-09-02 21:59:05 +0200

    test/test_vm_page_fill: fix copyright notice

--- test/test_vm_page_fill.c
+++ test/test_vm_page_fill.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 Richard Braun.
+ * Copyright (c) 2016 Richard Braun.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
