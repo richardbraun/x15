0a5af0a62ba2ed1c17404b1dd8ed15b0ff41abf7
Richard Braun <rbraun@sceen.net>
2017-06-17 18:45:31 +0200

    kern/cbuf: implement buffered reads and writes
    This change brings an interface for fast buffered accesses to the content
    of a circular buffer, and also an interface to write into a circular
    buffer at custom locations, in exchange for a small interface break
    of cbuf_read.

--- kern/cbuf.c
+++ kern/cbuf.c
@@ -17,6 +17,7 @@
 
 #include <assert.h>
 #include <stddef.h>
+#include <string.h>
 
 #include <kern/cbuf.h>
 #include <kern/error.h>
@@ -42,16 +43,21 @@ cbuf_index(const struct cbuf *cbuf, size_t abs_index)
     return abs_index & (cbuf->capacity - 1);
 }
 
+static void
+cbuf_update_start(struct cbuf *cbuf)
+{
+    /* Mind integer overflows */
+    if (cbuf_size(cbuf) > cbuf->capacity) {
+        cbuf->start = cbuf->end - cbuf->capacity;
+    }
+}
+
 void
 cbuf_push(struct cbuf *cbuf, char byte)
 {
     cbuf->buf[cbuf_index(cbuf, cbuf->end)] = byte;
     cbuf->end++;
-
-    /* Mind integer overflows */
-    if (cbuf_size(cbuf) > cbuf->capacity) {
-        cbuf->start = cbuf->end - cbuf->capacity;
-    }
+    cbuf_update_start(cbuf);
 }
 
 int
@@ -67,13 +73,76 @@ cbuf_pop(struct cbuf *cbuf, char *bytep)
 }
 
 int
-cbuf_read(const struct cbuf *cbuf, size_t index, char *bytep)
+cbuf_write(struct cbuf *cbuf, size_t index, const char *buf, size_t size)
 {
-    /* Mind integer overflows */
-    if ((cbuf->end - index - 1) >= cbuf_size(cbuf)) {
+    char *start, *end, *buf_end;
+    size_t new_end, skip;
+
+    if ((cbuf->end - index) > cbuf_size(cbuf)) {
         return ERROR_INVAL;
     }
 
-    *bytep = cbuf->buf[cbuf_index(cbuf, index)];
+    new_end = index + size;
+
+    if ((new_end - cbuf->start) > cbuf_size(cbuf)) {
+        cbuf->end = new_end;
+
+        if (size > cbuf_capacity(cbuf)) {
+            skip = size - cbuf_capacity(cbuf);
+            buf += skip;
+            index += skip;
+            size = cbuf_capacity(cbuf);
+        }
+    }
+
+    start = &cbuf->buf[cbuf_index(cbuf, index)];
+    end = start + size;
+    buf_end = cbuf->buf + cbuf->capacity;
+
+    if (end > buf_end) {
+        skip = buf_end - start;
+        memcpy(start, buf, skip);
+        buf += skip;
+        start = cbuf->buf;
+        size -= skip;
+    }
+
+    memcpy(start, buf, size);
+    cbuf_update_start(cbuf);
+    return 0;
+}
+
+int
+cbuf_read(const struct cbuf *cbuf, size_t index, char *buf, size_t *sizep)
+{
+    const char *start, *end, *buf_end;
+    size_t size;
+
+    size = cbuf->end - index;
+
+    /* At least one byte must be available */
+    if ((size - 1) >= cbuf_size(cbuf)) {
+        return ERROR_INVAL;
+    }
+
+    if (*sizep > size) {
+        *sizep = size;
+    }
+
+    start = &cbuf->buf[cbuf_index(cbuf, index)];
+    end = start + *sizep;
+    buf_end = cbuf->buf + cbuf->capacity;
+
+    if (end <= buf_end) {
+        size = *sizep;
+    } else {
+        size = buf_end - start;
+        memcpy(buf, start, size);
+        buf += size;
+        start = cbuf->buf;
+        size = *sizep - size;
+    }
+
+    memcpy(buf, start, size);
     return 0;
 }
--- kern/cbuf.h
+++ kern/cbuf.h
@@ -92,12 +92,25 @@ void cbuf_push(struct cbuf *cbuf, char byte);
 int cbuf_pop(struct cbuf *cbuf, char *bytep);
 
 /*
- * Read a byte at a specific location.
+ * Write into a circular buffer at a specific location.
  *
  * If the given index is outside buffer boundaries, ERROR_INVAL is returned.
- * Otherwise the byte is stored at the bytep address and 0 is returned.
- * The buffer isn't changed by this operation.
+ * Otherwise size bytes are copied into the circular buffer. If the range
+ * in the circular buffer goes beyond its end, the end index is updated as
+ * appropriate. If the buffer is full when extending its end, the oldest
+ * bytes are overwritten and the start index is updated accordingly.
  */
-int cbuf_read(const struct cbuf *cbuf, size_t index, char *bytep);
+int cbuf_write(struct cbuf *cbuf, size_t index, const char *buf, size_t size);
+
+/*
+ * Read from a circular buffer at a specific location.
+ *
+ * If the given index is outside buffer boundaries, ERROR_INVAL is returned.
+ * Otherwise at most *sizep bytes are copied into the given character buffer,
+ * and *sizep is updated to the number of bytes actually copied.
+ *
+ * The circular buffer isn't changed by this operation.
+ */
+int cbuf_read(const struct cbuf *cbuf, size_t index, char *buf, size_t *sizep);
 
 #endif /* _KERN_CBUF_H */
