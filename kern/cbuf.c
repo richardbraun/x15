83c826a255628cbb7371b17ead18ecc06ad22cd2
Richard Braun <rbraun@sceen.net>
2017-06-19 21:14:12 +0200

    kern/cbuf: update copyright year

--- kern/cbuf.c
+++ kern/cbuf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Richard Braun.
+ * Copyright (c) 2015-2017 Richard Braun.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
