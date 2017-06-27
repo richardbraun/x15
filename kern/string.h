b2ee0f5a8adff2d8ec4ab43580c760a2cfbe0cd8
Richard Braun <rbraun@sceen.net>
2017-06-28 01:02:08 +0200

    kern/string: use the restrict keyword where appropriate

--- kern/string.h
+++ kern/string.h
@@ -20,13 +20,13 @@
 
 #include <stddef.h>
 
-void * memcpy(void *dest, const void *src, size_t n);
+void * memcpy(void * restrict dest, const void * restrict src, size_t n);
 void * memmove(void *dest, const void *src, size_t n);
 void * memset(void *s, int c, size_t n);
 int memcmp(const void *s1, const void *s2, size_t n);
 size_t strlen(const char *s);
-char * strcpy(char *dest, const char *src);
-size_t strlcpy(char *dest, const char *src, size_t n);
+char * strcpy(char * restrict dest, const char *restrict src);
+size_t strlcpy(char * restrict dest, const char * restrict src, size_t n);
 int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, size_t n);
 char * strchr(const char *s, int c);
