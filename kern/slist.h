981a3b27ec24d17a3c53c60154066b8ebbccf4b5
Richard Braun <rbraun@sceen.net>
2017-07-25 21:10:47 +0200

    kern/slist: fix comment

--- kern/slist.h
+++ kern/slist.h
@@ -347,9 +347,7 @@ slist_llsync_insert_tail(struct slist *list, struct slist_node *node)
 /*
  * Insert a node after another node.
  *
- * The prev argument is used to determine the insertion point. It may safely
- * denote the end of the given list, in which case the node is inserted at
- * the head of the list.
+ * The prev node must be valid.
  */
 static inline void
 slist_llsync_insert_after(struct slist *list, struct slist_node *prev,
