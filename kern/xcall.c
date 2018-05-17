7615ab33c6d9e0b858aa3f183095a83f7a1d0620
Richard Braun <rbraun@sceen.net>
2018-05-17 21:11:12 +0200

    kern/xcall: add missing dependency on syscnt_setup

--- kern/xcall.c
+++ kern/xcall.c
@@ -130,7 +130,8 @@ xcall_setup(void)
 INIT_OP_DEFINE(xcall_setup,
                INIT_OP_DEP(cpu_mp_probe, true),
                INIT_OP_DEP(thread_bootstrap, true),
-               INIT_OP_DEP(spinlock_setup, true));
+               INIT_OP_DEP(spinlock_setup, true),
+               INIT_OP_DEP(syscnt_setup, true));
 
 void
 xcall_call(xcall_fn_t fn, void *arg, unsigned int cpu)
